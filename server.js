2aec495b8a9a1dd485ae50598115e048895f479d
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2024-06-11 14:51:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,9 @@ app.get('/protected', authMiddleware, (req, res) => {
     // This route is protected and requires authentication
     res.status(200).json({ message: 'Authenticated' });
   });
-  
+  app.use('/',(req,res)=>{
+      res.send('welcome');
+  });
 io.on('connection', (socket) => {
     socket.on('joinRoom', ({ room }) => {
         socket.join(room);
