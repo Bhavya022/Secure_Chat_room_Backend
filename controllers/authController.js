d419799359cf347e3f8e7baaef90e07aeb20c460
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2024-06-11 16:58:26 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 const db = require('../config/db');
